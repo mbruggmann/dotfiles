273cac0827dbc4e543c2d73f1c0c9deba3ec6dfa
Marc Bruggmann <marcbr@spotify.com>
2015-08-15 11:38:52 +0200

    Dont install coreutils and spark.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -18,10 +18,9 @@ then
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
   fi
-  
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark
+brew install grc
 
 exit 0
