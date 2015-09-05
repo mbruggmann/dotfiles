0e040d0abb89f6a16560b749f99db742ef4d2581
Marc Bruggmann <marcbr@spotify.com>
2015-09-05 19:47:44 +0200

    Install and configure font for macvim.

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# install theme
 mkdir -p ~/.vim/colors
 ln -s -f  ~/.dotfiles/vim/colors/Marcbr.vim ~/.vim/colors/Marcbr.vim
+
+# install vundle plugins
 vim -i NONE -c VundleInstall -c quitall 2> /dev/null
+
+# install font
+mkdir -p ~/Library/Fonts
+cp ~/.dotfiles/vim/font/*.ttf ~/Library/Fonts
