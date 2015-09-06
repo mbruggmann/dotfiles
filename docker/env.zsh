94768d3554abbcf4a4c559c12b426a22645d7ef0
Marc Bruggmann <marcbr@spotify.com>
2015-09-06 13:23:17 +0200

    Install docker-machine instead of boot2docker.

--- docker/env.zsh
+++ docker/env.zsh
@@ -1,3 +1,2 @@
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/marcbr/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+# set up docker-machine env
+eval "$(docker-machine env dev)"
