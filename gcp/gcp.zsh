d40007a1955294f9f87af33760467cf3426f57a0
Marc Bruggmann <marcbr@spotify.com>
2015-11-02 14:09:55 +0100

    Configure google cloud sdk.

---
+++ gcp/gcp.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+source '/Users/marcbr/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables shell command completion for gcloud.
+source '/Users/marcbr/google-cloud-sdk/completion.zsh.inc'
